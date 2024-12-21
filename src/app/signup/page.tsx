6458e3a8b196337b721adcd837f376b80e682487
Saba Junaid <sabajunaid146@gmail.com>
2024-12-21 13:03:57 +0500

    Update page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -40,7 +40,7 @@ export default function SignUpPage() {
               </div>
               <div className="flex items-center mb-4">
                 <input type="checkbox" className="mr-2" />
-                <span>Remember me?</span>
+                <span>Remember me</span>
               </div>
               <button
                 type="submit"
